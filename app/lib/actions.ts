fa9d3a9c8c9d728373712b392a494545e7cdbdb6
hy <yoshikawahideyo@yoshikawahideyonoMacBook-Air.local>
2023-11-17 11:16:14 +0900

    edit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+
 
 const FormSchema = z.object({
   id: z.string(),
@@ -104,4 +106,20 @@ export async function deleteInvoice(id: string) {
     return { message: 'Detabase Error: Failed to Delete Invoice.' };
   }
 }
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
+  }
+}
+
+
 
